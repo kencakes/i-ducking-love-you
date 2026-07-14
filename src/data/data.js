84acd9e112ff5bf0986eaffc7da6aa1fdf5f20ac
kencakes <vansaet.kendra@gmail.com>
2026-07-14 18:30:47 +0200

    Add Efteling memory with corresponding image

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import memory1 from "../images/memory1.jpg";
 import memory2 from "../images/memory2.jpg";
+import memory3 from "../images/memory3.jpg";
 
 export const memories = [
   {
@@ -15,5 +16,12 @@ export const memories = [
     date: "05-10-2025",
     description:
       "That playful moment under the table still gives me butterflies. Thinking about it still makes me ache for you.",
+  },
+  {
+    img: memory3,
+    title: "Efteling",
+    date: "01-06-2026",
+    description:
+      "The park was beautiful, but my favorite part wasn't the attractions, it was every smile, every hug, and every moment we shared together. Turns out my favorite attraction has always been you.",
   },
 ];
